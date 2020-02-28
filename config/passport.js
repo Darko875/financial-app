7d3f425e5a83757d3d68de9c746b0d6daf29945d
Daniel Sá <danielviana18@gmail.com>
2020-02-28 19:16:03 +0000

    Model added

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
 // Load User model
-const User = require('../models/User');
+const User = mongoose.model('User')
 
 module.exports = function(passport) {
   passport.use(
