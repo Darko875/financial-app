0677151ca9c1006c515669680c7dd62d764e8bc7
Daniel Sá <danielviana18@gmail.com>
2020-02-29 15:18:57 +0000

    Deploy number 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
-const router = express.Router;
+
 require('./config/passport')(passport);
 
 
